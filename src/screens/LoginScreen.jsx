2b49a67c60652427a1f90307e8ea2d35503c64f6
joao696 <joao.faust@alunos.sc.senac.br>
2024-04-18 16:54:11 -0300

    inicial

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,39 +1,56 @@
-import { View } from "react-native";
-import {  Button, Text, TextInput } from "react-native-paper";
 import { useState } from "react";
+import { View, Image} from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
+import { styles } from "../config/styles";
 
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-
-export default function LoginScreen(navigation) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
-
-  function realizaLogin(){
+  function realizaLogin() {
     console.log("Fazer Login");
   }
 
-
   return (
-    <View>
-      <Text>Faça seu Login</Text>
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
 
-      <TextInput 
-      placeholder="Digite seu e-mail" 
-      onChangeText={setEmail}
-      value = {email}
-      />
-
-    <TextInput 
-      placeholder="Digite sua senha" 
-      onChangeText={setPassword}
-      value = {password}
-      secureTextEntry = ""
-      />
-
-      <Button onPress = {realizaLogin}></Button>
-      <Button onPress = {() => navigation.navigate("RegisterScreen")}>Faça seu cadastro</Button>
-
+        <Text
+          variant="headlineMedium"
+          style={{
+            textAlign: "center",
+            marginBottom: 20,
+          }}
+        >
+           <Image
+        source={require("../../assets/usuario.png")}
+        style={{ width: 200, height: 200, justifyContent: "flex-start", alignItems: "flex-start", }}
+      >Faça seu login</Image>
+     
+          Faça seu Login
+        </Text>
+        <TextInput
+          placeholder="Digite seu e-mail"
+          onChangeText={setEmail}
+          value={email}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          onChangeText={setSenha}
+          value={senha}
+          secureTextEntry // faz com que o campo seja senha com *
+          style={styles.input}
+        />
+        <View>
+          <Button onPress={realizaLogin} mode="contained">
+            Fazer Login
+          </Button>
+        </View>
+        <Button onPress={() => navigation.navigate("RegisterScreen")}>
+          Faça seu cadastro
+        </Button>
+      </View>
     </View>
   );
 }
